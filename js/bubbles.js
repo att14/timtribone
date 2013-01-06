a72ff6e0c3cd702e2eda6f5cfc73cc5f75ac5c3b
Andrew Tribone <atribone@gmail.com>
2013-01-06 12:15:18 -0800

    looks pretty good now

--- js/bubbles.js
+++ js/bubbles.js
@@ -3,65 +3,75 @@ jQuery(document).ready(function () {
     var images = [],
         mouseX,
         mouseY,
-        createAndMoveImages = function (e) {
-            var createImageDiv = function () {
-                var createImage = function () {
-                    var imageNode, heightWidthRatio;
-
-                    imageNode = document.createElement('img');
-                    imageNode.src = 'http://littleguurrl.files.' +
-                        'wordpress.com/2012/01/598270.jpg?w=584';
-
-                    heightWidthRatio = imageNode.height / imageNode.width;
-                    imageNode.height = Math.random() * 100;
-                    imageNode.width = imageNode.height * heightWidthRatio;
-
-                    return imageNode;
-                },
-                    imageNode = createImage(),
-                    imageDivNode = document.createElement('div');
-
-                imageDivNode.appendChild(imageNode);
-
-                imageDivNode.className = 'image'
-                imageDivNode.style.position = 'absolute';
-                imageDivNode.style.left = mouseX - imageNode.width;
-                imageDivNode.style.top = mouseY - imageNode.height;
-
-                return imageDivNode;
-            },
-                moveImages = function () {
-                    $('.image').each(function () {
-                        var leftRight = Math.random() > .5 ? true : false,
-                            moveY = '-=30',
-                            moveX = leftRight ? '-=10' : '+=10';
-
-                        $(this).animate(
-                            {
-                                top: moveY,
-                                left: moveX
-                            },
-                            1
-                        );
-                        if(parseInt(this.style.top) < 0) {
-                            this.className = 'del';
-                        }
-                    });
-
-                    $('.del').remove();
-                },
-                imageDiv = createImageDiv();
-
-            document.body.appendChild(imageDiv);
-            moveImages()
-
-            setTimeout(createAndMoveImages, 50);
-        };
+        createImage,
+        moveImages;
 
     $(document).mousemove(function (e) {
         mouseX = e.pageX;
         mouseY = e.pageY;
     });
 
-    createAndMoveImages();
+    createImage = function () {
+        var createImageDiv, imageDiv;
+
+        createImageDiv = function () {
+            var createImage,
+                imageNode,
+                imageDivNode = document.createElement('div');
+
+            createImage = function () {
+                var imageNode, heightWidthRatio;
+
+                imageNode = document.createElement('img');
+                imageNode.src = 'http://littleguurrl.files.' +
+                    'wordpress.com/2012/01/598270.jpg?w=584';
+
+                heightWidthRatio = imageNode.height / imageNode.width;
+                imageNode.height = 10;
+                imageNode.width = imageNode.height * heightWidthRatio;
+
+                return imageNode;
+            };
+            imageNode = createImage();
+
+            imageDivNode.appendChild(imageNode);
+
+            imageDivNode.className = 'image'
+            imageDivNode.style.position = 'absolute';
+            imageDivNode.style.left = mouseX - imageNode.width;
+            imageDivNode.style.top = mouseY - imageNode.height;
+
+            return imageDivNode;
+        };
+
+        imageDiv = createImageDiv();
+        document.body.appendChild(imageDiv);
+
+        setTimeout(createImage, 75);
+    };
+
+    moveImages = function () {
+        $('.image').each(function () {
+            var leftRight =  Math.floor(Math.random() * mouseY) % 2 == 0 ? true : false,
+                moveY = '-=20',
+                moveX = leftRight ? '-=10' : '+=10';
+
+            $(this).animate(
+                {
+                    top: moveY,
+                    left: moveX
+                },
+                15
+            );
+
+            if(parseInt(this.style.top) < 0) {
+                $(this).remove()
+            }
+        });
+
+        setTimeout(moveImages, 15);
+    };
+
+    createImage()
+    moveImages()
 });
